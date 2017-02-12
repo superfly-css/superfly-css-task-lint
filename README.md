e57e2d8c75efd75eda099f91bf0cacce1f1f7169
Ole Ersoy <ole.ersoy@gmail.com>
2017-02-12 15:31:13 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # superfly-css-task-lint
+
+Lint task for superfly-css modules
+
+## Installation
+
+See [Installation](https://github.com/superfly-css/superfly-css/#installation).
+
+## Usage
+
+See [Usage](https://github.com/superfly-css/superfly-css/#usage)
---
+++ index.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var postcss_reporter = require('postcss-reporter');
+var stylelint = require('stylelint');
+var stylelint_config = require('stylelint-config-standard');
+
+var PLI = require('superfly-css-pli');
+
+var processors = [stylelint(stylelint_config), postcss_reporter({
+  clearMessages: true
+})];
+
+gulp.task('lint:css', function() {
+  gulp.src(PLI.TARGET_MAIN_CSS)
+    .pipe(postcss(linter))
+    .pipe(gulp.dest(PLI.target.main.css));
+
+  return gulp.src(PLI.TARGET_TEST_CSS)
+    .pipe(postcss(linter))
+    .pipe(gulp.dest(PLI.target.test.css));
+});
